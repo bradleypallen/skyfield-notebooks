a4b6469d2b5ca5dceeeb45f84ac6be81e0a794b2
Bradley P. Allen <bradley.p.allen@gmail.com>
2019-06-23 08:28:58 -0700

    Finished satpred docstrings
    Also changed timezone_str to timezone.

--- satpred.py
+++ satpred.py
@@ -23,53 +23,84 @@ class SatellitePass:
     """A representation of a satellite pass.
 
     Args:
-        sat:
-        topos:
-        timezone_str:
-        start_time:
-        culmination_time:
-        end_time:
-        pass_type:
-        peak_magnitude:
+        sat: A skyfield.sgp4lib.EarthSatellite instance representing the
+            the satellite.
+        topos: A skyfield.toposlib.Topos instance representing the location of
+            the observer.
+        timezone: A string describing the time zone that the topos is in, e.g.
+            'Europe/Paris'.
+        start_time: A skyfield.timelib.Time instance representing the moment
+            the satellite pass begins.
+        culmination_time: A skyfield.timelib.Time instance representing the moment
+            during the pass that the satellite has maximum altitude.
+        end_time: A skyfield.timelib.Time instance representing the moment
+            the satellite pass ends.
+        pass_type: A string representing the type of the pass, i.e. whether it
+            is during the daytime, during the night but eclipsed and hence
+            not visible, or during the night and visible for at least some
+            of the pass.
+        peak_magnitude: A float representing the estimated apparent magnitude of
+            the satellite during the pass.
 
     Attributes:
-       satellite:
-       topos:
-       start_time:
-       start_position:
-       culmination_time:
-       culmination_position:
-       end_time:
-       end_position:
-       direction_names:
-       direction_degrees:
-       date:
-       timezone:
-       start:
-       start_alt:
-       start_az:
-       start_d:
-       culm:
-       culm_alt:
-       culm_az:
-       culm_d:
-       end:
-       end_alt:
-       end_az:
-       end_d:
-       pass_type:
-       peak_magnitude:
+        sat: A skyfield.sgp4lib.EarthSatellite instance representing the
+            the satellite.
+        topos: A skyfield.toposlib.Topos instance representing the location of
+            the observer.
+        start_time: A skyfield.timelib.Time instance representing the moment
+            the satellite pass begins.
+        start_position: A skyfield.positionlib.Geocentric instance representing
+            the position of the satellite at the start of the pass.
+        culmination_time: A skyfield.timelib.Time instance representing the moment
+            during the pass that the satellite has maximum altitude.
+        culmination_position: A skyfield.positionlib.Geocentric instance representing
+            the position of the satellite at the culmination of the pass.
+        end_time: A skyfield.timelib.Time instance representing the moment
+            the satellite pass ends.
+        end_position: A skyfield.positionlib.Geocentric instance representing
+            the position of the satellite at the end of the pass.
+        direction_names: A list of strings naming the various compass directions.
+        direction_degrees: A list of float representing the various compass
+            directions in degrees.
+        date: A string that is the date of the start of the pass in ISO 8601
+            extended format.
+        timezone: A string describing the time zone that the topos is in.
+        start: A string that is the local time of the start of the pass in ISO 8601
+            extended format.
+        start_alt: A float that is the altitude of the satillite at the start of
+            the pass in degrees.
+        start_az: A string that is the name of the direction closest to the azimuth
+            of the satellite at the start of the pass.
+        start_d: A float that is the distance in kilometers of the satellite at
+            the start of the pass.
+        culm: A string that is the local time of the culmination of the pass in
+            ISO 8601 extended format.
+        culm_alt: A float that is the altitude of the satillite at the culmination
+            of the pass in degrees.
+        culm_az: A string that is the name of the direction closest to the azimuth
+            of the satellite at the culmination of the pass.
+        culm_d: A float that is the distance in kilometers of the satellite at
+            the culmination of the pass.
+        end: A string that is the local time of the end of the pass in ISO 8601
+            extended format.
+        end_alt: A float that is the altitude of the satillite at the end of
+            the pass in degrees.
+        end_az: A string that is the name of the direction closest to the azimuth
+            of the satellite at the end of the pass.
+        end_d: A float that is the distance in kilometers of the satellite at
+            the end of the pass.
+        pass_type: A string representing the type of the pass, i.e. whether it
+            is during the daytime, during the night but eclipsed and hence
+            not visible, or during the night and visible for at least some
+            of the pass.
+        peak_magnitude: A float representing the estimated apparent magnitude of
+            the satellite during the pass.
     """
 
-    def __init__(self, sat, topos, timezone_str, start_time, culmination_time,
+    def __init__(self, sat, topos, timezone, start_time, culmination_time,
                  end_time, pass_type, peak_magnitude):
 
         difference = sat - topos
-
-        # These instance variables contain the main information describing
-        # the satellite pass, represented using values of types
-        # skyfield.sgp4lib.EarthSatellite, skyfield.toposlib.Topos,
-        # skyfield.timelib.Time, and skyfield.positionlib.Geocentric.
 
         self.satellite = sat
         self.topos = topos
@@ -80,23 +111,18 @@ class SatellitePass:
         self.end_time = end_time
         self.end_position = difference.at(end_time)
 
-        # These additional instance variables represent information useful in
-        # displaying pass information succinctly to a human reader, represented
-        # using floats and strings, in emulation of those shown in tables and
-        # charts on the Heavens Above web site.
-
-        start_local_datetime = self.start_time.astimezone(pytz.timezone(timezone_str))
+        start_local_datetime = self.start_time.astimezone(pytz.timezone(timezone))
         start_alt, start_az, start_d = self.start_position.altaz('standard')
-        culm_local_datetime = self.culmination_time.astimezone(pytz.timezone(timezone_str))
+        culm_local_datetime = self.culmination_time.astimezone(pytz.timezone(timezone))
         culm_alt, culm_az, culm_d = self.culmination_position.altaz('standard')
-        end_local_datetime = self.end_time.astimezone(pytz.timezone(timezone_str))
+        end_local_datetime = self.end_time.astimezone(pytz.timezone(timezone))
         end_alt, end_az, end_d = self.end_position.altaz('standard')
-
         self.direction_names = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE',
                                 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N']
         self.direction_degrees = [ i*22.5 for i in range(17) ]
+
         self.date = start_local_datetime.isoformat(' ', timespec='seconds')[:10]
-        self.timezone = timezone_str
+        self.timezone = timezone
         self.start = start_local_datetime.isoformat(' ', timespec='seconds')[11:19]
         self.start_alt = int(np.round(start_alt.degrees))
         self.start_az = self._direction(start_az.degrees)
@@ -133,6 +159,17 @@ class SatellitePass:
 
 
     def to_json(self):
+        """Returns the data in the instance as a JSON dict.
+
+        Useful when converting a satpred.SatelliteEphemeris to a pandas.DataFrame.
+
+        Args:
+            None.
+
+        Returns:
+            A dictionary containing keys and values cooresponding to the
+                instance variables of the SatellitePass and their values.
+        """
         return {
             'satellite': self.satellite,
             'topos': self.topos,
@@ -162,35 +199,38 @@ class SatellitePass:
 
 
 class SatelliteEphemeris:
+    """Create a SatelliteEphemeris that describes passes for
+    a given satellite, observer at a specific topos, a start and end time,
+    data describing the positions of the Earth and Sun, and whether
+    only visible passes or all passes should be included.
+
+    Args:
+        t_0:  A skyfield.api.Time object that is the start time for the period
+            to search for passes.
+        t_1: A skyfield.api.Time object that is the end time for the period
+            to search for passes.
+        sat: A skyfield.sgp4lib.EarthSatellite object.
+        topos: A skyfield.toposlib.Topos object.
+        earth: A vector (obtained by loading an ephemeris file) supporting
+            the calculation of the position of Earth relative to
+            the Solar System barycenter.
+        sun: A vector (obtained by loading an ephemeris file) supporting
+            the calculation of the position of the Sun relative to
+            the Solar System barycenter.
+        visible: If True, then only nighttime, uneclipsed passes will be
+            returned, otherwise all passes with culmination
+            altitude > 10 degrees will be returned.
+
+    Attributes:
+        passes: A list of satpred.SatellitePass instances.
+        pass_types: A list of strings naming the various pass types.
+
+    Returns:
+        An instance of a SatelliteEphemeris class.
+    """
+
 
     def __init__(self, t_0, t_1, sat, topos, earth, sun, visible=True):
-        """Create a SatelliteEphemeris that describes passes for
-        a given satellite, observer at a specific topos, a start and end time,
-        data describing the positions of the Earth and Sun, and whether
-        only visible passes or all passes should be included.
-
-        Args:
-            t_0:  A skyfield.api.Time object that is the start time for the period
-                to search for passes.
-            t_1: A skyfield.api.Time object that is the end time for the period
-                to search for passes.
-            sat: A skyfield.sgp4lib.EarthSatellite object.
-            topos: A skyfield.toposlib.Topos object.
-            earth: A vector (obtained by loading an ephemeris file) supporting
-                the calculation of the position of Earth relative to
-                the Solar System barycenter.
-            sun: A vector (obtained by loading an ephemeris file) supporting
-                the calculation of the position of the Sun relative to
-                the Solar System barycenter.
-            visible: If True, then only nighttime, uneclipsed passes will be
-                returned, otherwise all passes with culmination
-                altitude > 10 degrees will be returned.
-
-        Attributes:
-
-        Returns:
-            An instance of a SatelliteEphemeris class.
-        """
         self.sat_minimum_observable_altitude = 10.0
         self.sat_pass_rough_period = 0.01
         time, _ = almanac.find_discrete(t_0, t_1,
@@ -268,6 +308,6 @@ class SatelliteEphemeris:
 
         Returns:
             A pandas.DataFrame object.
-            
+
         """
         return pd.DataFrame([p.to_json() for p in self.passes])
